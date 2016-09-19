2587d925aa6eff74a3440321707aa2d0de0fde88
Bill <brubaker.bill@gmail.com>
2016-09-19 18:53:38 -0500

    got a led working
    dims slightly, sensor needs work (not very good)

--- WristDevil.ino
+++ WristDevil.ino
@@ -13,28 +13,44 @@
 const int TRIGGER_PIN = 12;
 const int ECHO_PIN = 11;
 const int MAX_DISTANCE = 255; //Set to 255 for LED dimming test.
+const int MAX_BRIGHT = 255;
 NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
+const int SAMPLE_SIZE = 20;
+
+int avg = 1;
+int LED = 6;
+int brightness = 0;
+const int MAX_BRIGHT = 255;
 
 //Variables/consts for led pin
 //TODO: int LED //Assign ~PWN pin from board.
 //TODO: unsigned long brightness //Init to 0.
 //TODO: unsigned long fadeAmount //TODO calculate fadeAmount. NOTE: may not need this variable.
 
+int rollingAverage(int average, int newSample){
+  average -= average/SAMPLE_SIZE;
+  average += newSample/SAMPLE_SIZE;
+  return average;
+}
+
 void setup()
 {
-  //pinMode(LED, OUTPUT);
+  pinMode(LED, OUTPUT);
   Serial.begin(115200);
 }
 
 void loop()
 {
-  analogWrite(LED, brightness);
+  analogWrite(LED, avg);
 
   int uS = sonar.ping_cm();
-  brightness = uS;
+  brightness = MAX_BRIGHT - uS;
+  avg = rollingAverage(avg, brightness);
+
+  delay(30);
 
   //Print statements for debug.
   Serial.print("Ping: "); Serial.print(uS); Serial.println("cm :: ");
-  Serial.print("LED Brightness: "); Serial.println(brightness);
+  Serial.print("LED Brightness: "); Serial.println(avg);
 
 }
